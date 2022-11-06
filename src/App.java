16225eaef5e99694f128213e2e62acaec5fdd28c
MOUTET Adrien <adrien.moutet@etu.univ-amu.fr>
2022-11-06 17:01:35 +0100

    update

--- src/App.java
+++ src/App.java
@@ -11,44 +11,13 @@ import javafx.scene.shape.StrokeLineJoin;
 import javafx.stage.Stage;
 import java.util.Calendar;
 import java.util.Date;
+
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        MyTileset tileset = new MyTileset("tileset");
-        VBox master = new VBox();
-        //init a listview
-        ListView<MyTile> listview = new ListView<MyTile>();
-        ObservableList<MyTile> items = listview.getItems();
-        Button addButton = new Button("Add");
-        addButton.setOnAction(e->{
-            MyTile tile = new MyTile("tile", 32,32,10);
-            tileset.addTile(tile);
-            list.getItems().add(tile);
-        });
-        
-
-        //create a canvas
-        int scale = 10;
-        MyCanvas myCanvas = new MyCanvas(32, 32, scale);
-        ColorPicker colorPicker = new ColorPicker();
-        Button clearButton = new Button("Clear");
-        clearButton.setOnAction(e -> {
-            //gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        });
-        colorPicker.setOnAction(e -> {
-            myCanvas.setColor(colorPicker.getValue());
-        });
-        Spinner<Integer> spinner = new Spinner<Integer>(1, 10, 1);
-        spinner.valueProperty().addListener((obs, oldValue, newValue) -> {
-            //myCanvas.setScale(newValue);
-        });
-        ToolBar toolBar = new ToolBar(colorPicker, clearButton, spinner);
-        BorderPane root = new BorderPane();
-        root.setTop(toolBar);
-        root.setCenter(myCanvas);
-        root.setStyle("-fx-background-color: #000000;");
-        Scene scene = new Scene(root, 500, 500);
+        MyCanvas canvas = new MyCanvas(10, 10);
+        Scene scene = new Scene(canvas, 500, 500);
         stage.setScene(scene);
         stage.setTitle("Drawing Application");
         stage.show();
