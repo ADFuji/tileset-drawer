e46ab19f8da9f7d73989c0ba4df4146ba220b43c
MOUTET Adrien <adrien.moutet@etu.univ-amu.fr>
2022-11-14 09:42:55 +0100

    update view

--- src/main/java/studio/fuji/App.java
+++ src/main/java/studio/fuji/App.java
@@ -1,4 +1,5 @@
 package studio.fuji;
+
 //import stage
 import javafx.stage.Stage;
 import javafx.application.Application;
@@ -6,15 +7,19 @@ import javafx.beans.Observable;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import java.io.IOException;
+import java.util.Vector;
+
 //import button from javafx
 import javafx.scene.control.Button;
 //import scrollpane from javafx
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Slider;
 
 /**
  * JavaFX App
@@ -22,120 +27,95 @@ import javafx.scene.control.ScrollPane;
 public class App extends Application {
     private static MyTileset tileset;
     private static Scene scene;
+    private static MyTile selectedTile;
 
-    /*
     @Override
     public void start(Stage stage) throws IOException {
-        MyTileset tileset = new MyTileset("tileset");
-        MyTile tile = new MyTile(32, 32);
-        MyTile tile2 = new MyTile(32, 32);
-        MyTile tile3 = new MyTile(32, 32);
-        MyTile tile4 = new MyTile(32, 32);
-        MyTile tile5 = new MyTile(32, 32);
-        MyTile tile6 = new MyTile(32, 32);
-        tileset.addTile(tile);
-        tileset.addTile(tile2);
-        tileset.addTile(tile3);
-        tileset.addTile(tile4);
-        tileset.addTile(tile5);
-        tileset.addTile(tile6);
-
-        
-
-        VBox master = new VBox();
-        StackPane detail = new StackPane();
-        detail.getChildren().add(tileset.getTile(0).getCanvas());
-        
-        for(int i=0; i<tileset.getTileCount(); i++){
-            MyTile t = tileset.getTile(i);
-            t.setOnMouseClicked(event->{
-                detail.getChildren().remove(0);
-                detail.getChildren().add(t.getCanvas());
-            });
-            master.getChildren().add(t);
-        }
-        
-
-        GridPane grid = new GridPane();
-        grid.add(master, 0, 0);
-        grid.add(detail, 1, 0);
-        scene = new Scene(grid, 640, 640);
-        //if i press s
-        scene.setOnKeyPressed(event->{
-            switch(event.getCode()){
-                case S:
-                    tileset.saveAs("tileset.png");
-                    break;
-            }
-        });
-        stage.setScene(scene);
-        stage.show();
-    }
-    */
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    @Override
-    public void start(Stage stage) throws IOException{
         tileset = new MyTileset("tileset");
         BorderPane main_layout = new BorderPane();
+        BorderPane detail_layout = new BorderPane();
+        ScrollPane ddetail = new ScrollPane();
         StackPane detail = new StackPane();
         detail.getChildren().add(new Label("Detail"));
-        detail.setStyle("-fx-background-color: #ffffff");
-        
-        VBox master = new VBox();
+        ddetail.setContent(detail);
+        detail_layout.setCenter(ddetail);
+
+        HBox control = new HBox();
+        Slider slider = new Slider(0, 100, 1);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+        control.getChildren().add(slider);
+        detail_layout.setBottom(control);
+
+        BorderPane master = new BorderPane();
         {
             VBox tiles = new VBox();
             ScrollPane scroll = new ScrollPane();
             scroll.setContent(tiles);
             scroll.setFitToWidth(true);
             scroll.setFitToHeight(true);
-            master.getChildren().add(scroll);
+            scroll.setPrefHeight(500);
+            // change the size of the current tile
+            slider.valueProperty().addListener((Observable o) -> {
+                Vector<MyTile> tileList = tileset.getTiles();
+                for (int i = 0; i < tileList.size(); i++) {
+                    tileList.get(i).getCanvas().setSize((int) slider.getValue());
+                }
+            });
+            detail_layout.setOnScroll(e -> {
+                System.out.println(e);
+                // if i press shift and scroll, i change the size of the current tile
+                if (e.isShiftDown()) {
+                    slider.setValue(slider.getValue() + e.getDeltaY());
+                }
+                // if press ctrl and scroll, i change x axis of ddetail
+                if (e.isControlDown()) {
+                    ddetail.setHvalue(ddetail.getHvalue() + e.getDeltaY() / 1000);
+                }
+                // if press alt and scroll, i change y axis of ddetail
+                if (e.isAltDown()) {
+                    ddetail.setVvalue(ddetail.getVvalue() + e.getDeltaY() / 1000);
+                }
+            });
+            master.setTop(scroll);
             HBox buttons = new HBox();
             {
                 Button new_tile = new Button("New Tile");
-                new_tile.setOnAction(event->{
+                new_tile.setOnAction(event -> {
                     MyTile tile = new MyTile(32, 32);
-                    tile.setOnMouseClicked(event2->{
+                    tile.setOnMouseClicked(event2 -> {
                         detail.getChildren().remove(0);
                         detail.getChildren().add(tile.getCanvas());
+                        selectedTile = tile;
                     });
                     tileset.addTile(tile);
                     tiles.getChildren().add(tile);
                 });
                 Button rem_tile = new Button("Remove Tile");
-                rem_tile.setOnAction(event->{
-                    if(tileset.getTileCount()>0){
-                        tileset.removeTile(tileset.getTileCount()-1);
-                        tiles.getChildren().remove(tiles.getChildren().size()-1);
+                rem_tile.setOnAction(event -> {
+                    if (tileset.getTileCount() > 0) {
+                        tileset.removeTile(tileset.getTileCount() - 1);
+                        tiles.getChildren().remove(tiles.getChildren().size() - 1);
                     }
                 });
                 buttons.getChildren().addAll(new_tile, rem_tile);
             }
-            GridPane grid = new GridPane();
-            grid.add(scroll, 0, 0);
-            grid.add(buttons, 0, 1);
-            grid.setPrefHeight(640);
-            grid.setPrefWidth(200);
-            //change the size of the scroll cell to fit the content
-            scroll.setPrefHeight(640);
-            scroll.setPrefWidth(200);
-            
-            master.getChildren().add(grid);
+            master.setBottom(buttons);
+            // master display flex column justify-content space-between
+            master.setStyle("-fx-display: flex; -fx-flex-direction: column; -fx-justify-content: space-between");
         }
-        master.setStyle("-fx-background-color: #0000ff");
-        //change the width of the master
+        // change the width of the master
         master.setPrefWidth(200);
-        
+
         main_layout.setLeft(master);
-        main_layout.setCenter(detail);
-
-
+        main_layout.setCenter(detail_layout);
 
         scene = new Scene(main_layout, 640, 640);
         stage.setScene(scene);
         stage.show();
+
     }
+
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/studio/fuji/MyTileset.java
+++ src/main/java/studio/fuji/MyTileset.java
@@ -13,43 +13,54 @@ public class MyTileset {
         this.name = name;
         this.tiles = new Vector<MyTile>();
     }
+
     public void addTile(MyTile tile) {
         this.tiles.add(tile);
     }
+
     public void addTile(int width, int height) {
         this.tiles.add(new MyTile(width, height));
     }
+
     public void removeTile(MyTile tile) {
         this.tiles.remove(tile);
     }
+
     public void removeTile(int index) {
         this.tiles.remove(index);
     }
+
     public MyTile getTile(int index) {
         return this.tiles.get(index);
     }
+
     public int getTileCount() {
         return this.tiles.size();
     }
+
     public String getName() {
         return this.name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public BufferedImage getImage(){
-        BufferedImage image = new BufferedImage(tiles.get(0).getWidth()*4, tiles.get(0).getWidth()*2, BufferedImage.TYPE_INT_RGB);
+
+    public BufferedImage getImage() {
+        BufferedImage image = new BufferedImage(tiles.get(0).getWidth() * 4, tiles.get(0).getWidth() * 2,
+                BufferedImage.TYPE_INT_RGB);
         int lastX = 0;
         int lastY = 0;
-        for(int i = 0; i < this.tiles.size(); i++) {
+        for (int i = 0; i < this.tiles.size(); i++) {
             BufferedImage tileImage = this.tiles.get(i).getImage();
-            if(tileImage.getWidth()+lastX>image.getWidth()) {
+            if (tileImage.getWidth() + lastX > image.getWidth()) {
                 lastX = 0;
                 lastY += tileImage.getHeight();
             }
-            System.out.println("Tile " + i + " is " + tileImage.getWidth() + "x" + tileImage.getHeight()+ " at " + lastX + ", " + lastY);
-            for(int x = 0; x < tileImage.getWidth(); x++) {
-                for(int y = 0; y < tileImage.getHeight(); y++) {
+            System.out.println("Tile " + i + " is " + tileImage.getWidth() + "x" + tileImage.getHeight() + " at "
+                    + lastX + ", " + lastY);
+            for (int x = 0; x < tileImage.getWidth(); x++) {
+                for (int y = 0; y < tileImage.getHeight(); y++) {
                     image.setRGB(x + lastX, y + lastY, tileImage.getRGB(x, y));
                 }
             }
@@ -57,18 +68,24 @@ public class MyTileset {
         }
         return image;
     }
+
     public int[] calcSize() {
         int[] size = new int[2];
-        size[0] = this.tiles.get(0).getWidth()*getTileCount();
+        size[0] = this.tiles.get(0).getWidth() * getTileCount();
         size[1] = this.tiles.get(0).getHeight();
         return size;
     }
-    public void saveAs(String path){
+
+    public void saveAs(String path) {
         BufferedImage image = this.getImage();
         try {
             javax.imageio.ImageIO.write(image, "png", new java.io.File(path));
         } catch (java.io.IOException e) {
             System.out.println("Error saving image");
         }
+    }
+
+    public Vector<MyTile> getTiles() {
+        return this.tiles;
     }
 }
